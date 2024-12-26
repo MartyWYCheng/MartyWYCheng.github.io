5f00897e30578bc4bfe46c2767a3aa0b0493ff2c
Marty Cheng <marty.jehng@gmail.com>
2024-12-26 11:31:25 -0500

    main import error fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import ("autopreview/react").then(({ default: AutoPreview }) => {
-  new AutoPreview("#root")})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
