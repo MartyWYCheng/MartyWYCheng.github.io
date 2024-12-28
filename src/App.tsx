a764fab2c22cf84e4c2dd2e755cb903672d92439
Marty Cheng <marty.jehng@gmail.com>
2024-12-27 20:42:33 -0500

    closed sidebar when site loads

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Resume } from './pages/Resume';
 import { ExperienceDetail } from './pages/ExperienceDetail';
+import { SkillsList } from './components/Sidebar/SkillsList';
 
 export default function App() {
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true); // Set initial state to true
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Resume />} />
-        <Route path="/experience/:id" element={<ExperienceDetail />} />
-      </Routes>
+      <div className="flex">
+        <SkillsList isCollapsed={isSidebarCollapsed} setIsCollapsed={setIsSidebarCollapsed} />
+        <div className={`flex-1 transition-all duration-300 ${isSidebarCollapsed ? 'pl-4' : 'md:pl-64'}`}>
+          <Routes>
+            <Route path="/" element={<Resume />} />
+            <Route path="/experience/:id" element={<ExperienceDetail />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 }
