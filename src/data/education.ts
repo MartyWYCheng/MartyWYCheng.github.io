e596f936f6d6b7a4d488625a7e9518b520dde116
Marty Cheng <marty.jehng@gmail.com>
2024-12-28 13:09:42 -0500

    education achievement list attempt 1

--- src/data/education.ts
+++ src/data/education.ts
@@ -3,20 +3,22 @@ export const educationData = [
     degree: 'PhD in Computer Science',
     school: 'Florida Atlantic University',
     period: '2024 - Present',
-    achievements: 'Successfully passed the qualifying examination',
+    achievements: ['Successfully passed the qualifying examination'],
   },
   {
     degree: 'Master of Science in Artificial Intelligence',
     school: 'Florida Atlantic University',
     period: '2022 - 2023',
-    achievements: 'Big Data Analytics Certificate',
+    achievements: ['Big Data Analytics Certificate'],
   },
   {
     degree: 'Bachelor of Science in Mechanical Engineering - Computer Science Minor',
     school: 'Florida Atlantic University',
     period: '2018 - 2021',
-    achievements: ['Computer Scicence Minor',
-    'FAU High School Dual Enrollment Program',
-    'Innovation Leadership Honors Program (ILHP)' ]
+    achievements: [
+      'Computer Science Minor',
+      'FAU High School Dual Enrollment Program',
+      'Innovation Leadership Honors Program (ILHP)'
+    ]
   },
 ];
