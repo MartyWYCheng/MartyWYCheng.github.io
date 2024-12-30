8f768124daa3a29c1d9021340c765482274d9196
Marty Cheng <marty.jehng@gmail.com>
2024-12-30 11:37:13 -0500

    project under construction message

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,4 +5,10 @@ export const projectsData = [
     description: 'Integrated an LLM AI chatbot with retrieval augmented generation (RAG) into an app developed from ground up in Swift.',
     skills: ['AI Integration', 'RAG', 'LLM', 'Swift', 'OpenAI API', 'Python', 'Git Version Control'],
   },
+  {
+    id: '',
+    title: 'Section Under Construction...',
+    description: 'This section is under construction. Please check back later for updates.',
+    skills: [],
+  },
 ];
