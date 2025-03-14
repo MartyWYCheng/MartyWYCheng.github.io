ec68b6f1482b4d7070f95a9310c3b8ffb1eac665
Marty Cheng <marty.jehng@gmail.com>
2025-03-13 21:17:15 -0400

    updated publication list

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -1,5 +1,12 @@
 export const publicationsData = [
   {
+    title: 'A natural biomimetic prosthetic hand with neuromorphic tactile sensing for precise and compliant grasping',
+    journal: 'Science Advances',
+    date: '2025',
+    authors: 'Sriramana Sankar, Wen-Yu Cheng, Jinghua Zhang, Ariel Slepyan, Mark M Iskarous, Rebecca J Greene, Rene DeBrabander, Junjun Chen, Arnav Gupta, Nitish V Thakor',
+    description: 'The human hand’s hybrid structure combines soft and rigid anatomy to provide strength and compliance for versatile object grasping. Tactile sensing by skin mechanoreceptors enables precise and dynamic manipulation. Attempts to replicate the human hand have fallen short of a true biomimetic hybrid robotic hand with tactile sensing. We introduce a natural prosthetic hand composed of soft robotic joints and a rigid endoskeleton with three independent neuromorphic tactile sensing layers inspired by human physiology. Our innovative design capitalizes on the strengths of both soft and rigid robots, enabling the hybrid robotic hand to compliantly grasp numerous everyday objects of varying surface textures, weight, and compliance while differentiating them with 99.69% average classification accuracy. The hybrid robotic hand with multilayered tactile sensing achieved 98.38% average classification accuracy in a …',
+    link: 'https://www.science.org/doi/pdf/10.1126/sciadv.adr9300'
+  },{
     title: 'Flexible multilayer tactile sensor on a soft robotic fingertip',
     journal: '2022 IEEE Sensors',
     date: '2022',
